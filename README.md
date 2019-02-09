5c9d5fef5384bbb879b73baea007c258ec5b45ba
mark_test <mark_test@CORP.AMAZONWORKSPACES.COM>
2019-02-09 16:31:47 -0500

    moved base.py to the next test_mark_0001 folder down

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # test_mark_0001
 This is a first attempt to use git and pipy to manage a hypertheticla module i want to build
 
-### I used the following websites to build a module and publish it to pipy.
+## I used the following websites to build a module and publish it to pipy.
 * #### https://pythonhosted.org/foo-test/minimal.html
 * #### https://docs.python.org/3/distutils/setupscript.html
 * #### https://packaging.python.org/tutorials/packaging-projects/
+
+
+## Things learned along the way.
+### this statment builds the wheels and eggs
+* ### python setup.py sdist bdist_wheel
+### documentation says to do this...
+* ### python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+### because i have dependencies AND i'm using TestPyPi I need to do this
+* ### pip install -i https://testpypi.python.org/pypi --extra-index-url https://pypi.python.org/pypi MODULENAME
---
+++ test_mark_0001/__init__.py
@@ -0,0 +1,29 @@
+"""
+_____________________________________________________________________________________________
+|                                                                                           |
+|       test_mark_0001 package: makes healthcare data analysis easy                         |
+|                                                                                           |
+| This package is to provide easy to use tools to aid in healthcare data cleaning and       |
+|  analyses. A full applied guide to the entire package may soon be available at:           |
+|       https://github.com/markregine/test_mark_0001                                        |
+|                                                                                           |
+| Current contents include:                                                                 |
+|       - Raw data cleaning tools                                                           |
+|       -                                                                                   |
+|       -                                                                                   |
+|       -                                                                                   |
+|___________________________________________________________________________________________|
+
+CONTENTS
+
+module1
+	description goes here
+
+module2
+	description goes here
+
+See http://some_website for a full guide to all the package features
+"""
+from .base import (print_hello, get_df)
+
+from .version import __version__
